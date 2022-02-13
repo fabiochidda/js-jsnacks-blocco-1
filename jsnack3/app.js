42f0e94697bdf911a8966b66449d7ff4f78d999c
fabiochidda <fabio.chidda99@gmail.com>
2022-02-13 21:37:20 +0100

    Creazione somma degli input con "while"

--- jsnack3/app.js
+++ jsnack3/app.js
@@ -1,9 +1,25 @@
+/*
 let inputSum = 0;
 
 for (i=0; i<5; i++) {
 
     const userInput = parseInt(prompt("Inserisci un numero"));
     inputSum = inputSum + userInput;
+
+}
+
+console.log(inputSum);
+*/
+
+let inputSum = 0;
+
+let i = 0;
+
+while (i<5) {
+
+    const userInput = parseInt(prompt("Inserisci un numero"));
+    inputSum = inputSum + userInput;
+    i++;
 
 }
 
